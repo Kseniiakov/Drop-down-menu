35bf866185c0a05a5dc58c9c196dc742093f2fa4
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-04-05 17:49:47 +0300

    Delete var 1

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,20 +1,5 @@
 "use strict";
 
-// var 1
-
-// const navList = document.querySelector('.nav-list');
-// const subNavList = document.querySelector('.sub-nav-list');
-
-// function linkClick() {
-//     this.children[1].classList.add('darkGreen');
-//     this.children[1].children[1].classList.add('display');
-// };
-
-// navList.addEventListener('click', linkClick);
-// subNavList.addEventListener('click', linkClick);
-
-// var 2
-
 const navList = document.querySelector('.nav-list');
 const subNavList = document.querySelector('.sub-nav-list');
 
