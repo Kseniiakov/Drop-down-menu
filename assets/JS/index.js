8895ecd93935568d035789e2b0c087b8cc463ff5
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-04-05 17:48:26 +0300

    Add new changes

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,12 +1,27 @@
 "use strict";
 
+// var 1
+
+// const navList = document.querySelector('.nav-list');
+// const subNavList = document.querySelector('.sub-nav-list');
+
+// function linkClick() {
+//     this.children[1].classList.add('darkGreen');
+//     this.children[1].children[1].classList.add('display');
+// };
+
+// navList.addEventListener('click', linkClick);
+// subNavList.addEventListener('click', linkClick);
+
+// var 2
+
 const navList = document.querySelector('.nav-list');
 const subNavList = document.querySelector('.sub-nav-list');
 
-function linkClick() {
-    this.children[1].classList.add('darkGreen');
-    this.children[1].children[1].classList.add('display');
-};
+function linkClick(event) {
+    this.classList.add('darkGreen');
+    this.children[1].classList.add('display');
+}
 
-navList.addEventListener('click', linkClick);
-subNavList.addEventListener('click', linkClick);
+navList.children[1].addEventListener('click', linkClick);
+subNavList.children[1].addEventListener('click', linkClick);
